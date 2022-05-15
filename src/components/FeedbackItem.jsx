1f99de70647649cdbd4ec6fbeb569c1edf2c9e61
Siskajan <siskajan@hotmail.com>
2022-05-15 17:03:51 +0200

    removed unused declaration

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 import { useContext } from 'react'
-import FeedbackContext, { FeedbackProvider } from '../context/FeedbackContext'
+import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackItem({item}) {
     const {deleteFeedback, editFeedback } = useContext(FeedbackContext)
